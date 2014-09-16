7b72eda8b156e826f0b4de85f5fc58c849a9d479
Marcel Hauf <mail@marcelhauf.name>
2014-09-16 15:19:02 +0200

    Add PrettyPrint to JsonFormatter

--- jsonformatter.go
+++ jsonformatter.go
@@ -8,13 +8,29 @@ import (
 // JsonFormatter
 // Note: Only supports encoding/json types as Entry values
 type JsonFormatter struct {
+	PrettyPrint bool
+	Indent      string
+	Prefix      string
 }
 
 func NewJsonFormatter() Formatter {
-	return &JsonFormatter{}
+	return &JsonFormatter{
+		PrettyPrint: true,
+		Indent:      "\t",
+		Prefix:      "",
+	}
 }
 
 func (f *JsonFormatter) Write(entry Entry, w io.Writer) error {
+	if f.PrettyPrint {
+		b, err := json.MarshalIndent(entry, f.Prefix, f.Indent)
+		if err != nil {
+			return err
+		}
+		_, err = w.Write(b)
+		return err
+	}
+
 	e := json.NewEncoder(w)
 	return e.Encode(entry)
 }
